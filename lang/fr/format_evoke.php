3444a095af839bbaa092cc62e9532e087bd7202d
Willian Mano <willian.mano@jove.com>
2023-03-31 15:06:30 -0300

    Add fr language

---
+++ lang/fr/format_evoke.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Strings for component Evoke course format.
+ *
+ * @package   format_evoke
+ * @copyright 2022 World Bank Group <https://worldbank.org>
+ * @author    Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+$string['addsections'] = 'Ajouter une mission';
+$string['currentsection'] = 'Cette mission';
+$string['editsection'] = 'Modifier la mission';
+$string['editsectionname'] = 'Modifier le nom de la mission';
+$string['deletesection'] = 'Supprimer la mission';
+$string['newsectionname'] = 'Nouveau nom pour la mission {$a}';
+$string['sectionname'] = 'Mission';
+$string['pluginname'] = 'Format Evoke';
+$string['section0name'] = 'Mission 0';
+$string['page-course-view-topics'] = 'Toute page principale de cours au format Evoke';
+$string['page-course-view-topics-x'] = 'Toute page de cours au format Evoke';
+$string['hidefromothers'] = 'Cacher la mission';
+$string['showfromothers'] = 'Afficher la mission';
+$string['privacy:metadata'] = 'Le plugin de format Evoke ne stocke aucune donnée personnelle.';
+
+$string['coursedisplay_hidesections'] = 'Masquer les sections pour les étudiants';
+
+$string['syllabus'] = 'Programme du cours';
+$string['syllabus_help'] = 'Programme détaillé du cours';
+
+$string['dologin'] = 'Se connecter pour s\'inscrire';
+$string['takethiscourse'] = 'Suivre ce cours';
+$string['backtocourse'] = 'Retour au cours';
+$string['enrolnotavailable'] = 'L\'inscription n\'est pas encore disponible';
+$string['category'] = 'Catégorie';
